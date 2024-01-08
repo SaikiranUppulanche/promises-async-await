6d9a27d6d8cf9129af2a5e2356f937934b243644
Saikiran Uppulanche <113796570+SaikiranUppulanche@users.noreply.github.com>
2024-01-08 23:46:43 +0530

    converted createPost, deletePost to async await

--- promise.js
+++ promise.js
@@ -9,21 +9,55 @@ function updateLastUserActivityTime() {
   });
 }
 
-function createPost(post) {
-  return new Promise((resolve, reject) => {
+async function createPost(post) {
+  const updatePost = await new Promise((resolve, reject) => {
     setTimeout(() => {
       if (!post) {
         reject("please enter post");
       }
       posts.push(post);
+      console.log(posts);
       resolve();
     }, 2000);
   });
+  return updatePost;
 }
 
-Promise.all([createPost({ title: "Post1" }), updateLastUserActivityTime()])
-  .then((val) => {
-    console.log(posts);
-    console.log(`user last activity time: ${val[1]}`);
-  })
-  .catch((err) => console.log(err));
+async function deletePost() {
+  return await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (posts.length > 0) {
+        let popped = posts.pop();
+        resolve(popped);
+      } else {
+        reject("Error: Something went wrong");
+      }
+    }, 1000);
+  });
+}
+
+function printPosts() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      posts.forEach((post) => {
+        console.log(post);
+      });
+    }, 0);
+  });
+}
+
+function addPost() {
+  createPost({ title: "Post" });
+  deletePost().catch((err) => console.log(err));
+}
+addPost();
+// createPost({ title: "Post" }).then(() =>
+//   deletePost().catch((err) => console.log(err))
+// );
+
+// Promise.all([createPost({ title: "Post1" }), updateLastUserActivityTime()])
+//   .then((val) => {
+//     console.log(posts);
+//     console.log(`user last activity time: ${val[1]}`);
+//   })
+//   .catch((err) => console.log(err));
